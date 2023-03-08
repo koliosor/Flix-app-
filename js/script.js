f87c3b55b5fc7069448923c7ef71cb645f171c7e
[Angelo Kolios] <49371480+koliosor@users.noreply.github.com>
2023-03-08 19:08:28 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: "",
+    type: "",
+    page: 1,
+    totalPages: 1,
+    totalResults: 0,
+  },
+  api: {
+    // Register your key at https://www.themoviedb.org/settings/api and enter here
+    // Only use this for development or very small projects. You should store your key and make requests from a server
+    apiKey: "3fd2be6f0c70a2a598f084ddfb75487c",
+    apiUrl: "https://api.themoviedb.org/3/",
+  },
 };
+
 async function displayPopularMovies() {
   const { results } = await fetchAPIData("movie/popular");
   // console.log(results);
@@ -164,7 +178,7 @@ async function displayShowDetails() {
       <i class="fas fa-star text-primary"></i>
       ${show.vote_average.toFixed(1)} / 10
     </p>
-    <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+    <p class="text-muted">Last Air Date: ${show.first_air_date}</p>
     <p>
       ${show.overview}
     </p>
@@ -222,6 +236,121 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Search movies/Shows
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  global.search.type = urlParams.get("type");
+  global.search.term = urlParams.get("search-term");
+  console.log(queryString);
+
+  if (global.search.term !== "" && global.search.term !== null) {
+    const { results, total_pages, page, total_results } = await searchAPIData();
+    console.log(results);
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
+
+    if (results.length === 0) {
+      showAlert("No results found");
+      return;
+    }
+    document.querySelector("#search-term").value = "";
+    displaySearchResults(results);
+  } else {
+    showAlert("please enter a search term", "error");
+  }
+}
+
+function displaySearchResults(results) {
+  // Clear Previous results
+
+  document.querySelector("#search-results").innerHTML = "";
+  document.querySelector("#search-results-heading").innerHTML = "";
+  document.querySelector("#pagination").innerHTML = "";
+
+  results.forEach((result) => {
+    const div = document.createElement("div");
+    div.innerHTML = `  <div class="card">
+    <a href="${global.search.type}-details.html?id=${result.id}">
+    ${
+      result.poster_path
+        ? `<img
+      src="https://image.tmdb.org/t/p/w500${result.poster_path}"
+      class="card-img-top"
+      alt="${global.search.type === "movie" ? result.title : result.name}"
+    />`
+        : `<img
+    src="../images/no-image.jpg"
+    class="card-img-top"
+    alt="${global.search.type === "movie" ? result.title : result.name}"
+  />`
+    }      </a>
+    <div class="card-body">
+      <h5 class="card-title">${
+        global.search.type === "movie" ? result.title : result.name
+      }</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${
+          global.search.type === "movie"
+            ? result.release_date
+            : result.first_air_date
+        }</small>
+      </p>
+    </div>
+  </div> `;
+    document.querySelector(
+      "#search-results-heading"
+    ).innerHTML = ` <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>`;
+    document.querySelector("#search-results").appendChild(div);
+  });
+  displayPagination();
+}
+
+// Create and Display Pagination for Search
+function displayPagination() {
+  const div = document.createElement("div");
+  div.classList.add("pagination");
+  div.innerHTML = `
+  <button class="btn btn-primary" id="prev">Prev</button>
+          <button class="btn btn-primary" id="next">Next</button>
+          <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+  `;
+  document.querySelector("#pagination").appendChild(div);
+
+  // Disable prev button if on first page
+  if (global.search.page === 1) {
+    document.querySelector("#prev").disabled = true;
+  }
+  // Disable next button if on last page
+
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector("#next").disabled = true;
+  }
+
+  // Νext Page
+  document.querySelector("#next").addEventListener("click", async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+  // Previous Page
+  document.querySelector("#prev").addEventListener("click", async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+}
+
+function showAlert(message, className = `error`) {
+  const alertEl = document.createElement("div");
+  alertEl.classList.add("alert", className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector("#alert").appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+}
+
 // Display Slider Movies
 async function displaySlider() {
   const { results } = await fetchAPIData("movie/now_playing");
@@ -266,8 +395,8 @@ function initSwiper() {
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-  const API_KEY = "f5cb07f0726c83f2283cc48f83baf609";
-  const API_URL = "https://api.themoviedb.org/3/";
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
   showSpinner();
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
@@ -276,6 +405,23 @@ async function fetchAPIData(endpoint) {
   const data = await response.json();
   hideSpinner();
   console.log(data);
+  return data;
+}
+
+// Make request to search
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
+  );
+
+  const data = await response.json();
+  hideSpinner();
+
   return data;
 }
 
